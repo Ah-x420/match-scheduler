7cf7dc35d952c8277274b656bd2474d0055b43cb
Ah-x420 <ahmad.lakky7@gmail.com>
2025-07-10 14:00:38 +0400

    Delete MatchPostpone - 98 directory

--- MatchPostpone - 98/src/matchpostpone/Postpone.java
+++
@@ -1,7 +0,0 @@
-package matchpostpone;
-
-
-public interface Postpone {
-    void postp();
-}
-// make the system postpone the matches to september onward to conserve player energy and water consumption
